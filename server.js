35e2488365eb2462c0ffc4a0602a421f730e2c63
rshorche <44744441+rshorche@users.noreply.github.com>
2023-12-25 15:22:31 +0330

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
+    '/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
